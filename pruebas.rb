c1f506b0ef3fd5edf111443aff88266e99b598e2
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-05-05 21:03:04 -0300

    Create pruebas.rb

---
+++ pruebas.rb
@@ -0,0 +1,17 @@
+#Primer ejercicio de Ruby haciendo pruebitas :D 
+
+Ciudades = %w[
+    Londres
+    Oslo
+    Paris
+    Amsterdam
+    Berlin
+]
+
+Visitado = %w[
+    Berlin
+    Oslo
+]
+
+puts "Aun necesito" + "visitar las" + "siguientes ciudades: ",
+Ciudades = Visitado
