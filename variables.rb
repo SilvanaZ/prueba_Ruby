0958922aa755156befcf277e1524b9ffec5eb1cf
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-06-04 23:21:50 -0300

    Adding Knoledge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+#nombre = "Silvana"
+
+#edad = 23
+
+#tutor = "Dami"
+
+#puts nombre
+
+#nombre_del_tutor = "Dami"
+
+#puts nombre_del_tutor
+
+#NO SE DEBE PONER LA PRIMERA LETRA DE LA VARIABLE EN MAYUSCULA.
+
